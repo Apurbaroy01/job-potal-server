45ea7be2f96b8389350ed61cae59f0abdf1b0878
Apurbaroy01 <apurbaroy00011@gmail.com>
2025-08-04 02:44:39 +0600

    2nd

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ app.use(express())
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://adrikaroy487:adrikaroy487@cluster0.xgolbpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_user}:${process.env.DB_pass}@cluster0.xgolbpd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,9 +26,19 @@ async function run() {
     try {
         await client.connect();
         console.log("You successfully connected to MongoDB✅!");
-    } catch(error){
+    } catch (error) {
         console.error("Error connecting to MongoDB❌:", error)
     }
+
+
+    const JobCollection = client.db("JobPotal").collection("jobs");
+
+    app.get('/jobs', async(req, res) => {
+        const result=await JobCollection.find().toArray()
+        res.send(result)
+    })
+
+
 }
 run();
 
